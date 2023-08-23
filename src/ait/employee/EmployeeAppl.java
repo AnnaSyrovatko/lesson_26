aa512e1ea64a9f49dea1dceca0aa4cb5605e94b8
Anna Syrovatko <annasyrovatko76@gmail.com>
2023-08-24 01:58:42 +0700

    homework

---
+++ src/ait/employee/EmployeeAppl.java
@@ -0,0 +1,51 @@
+package ait.employee;
+
+import ait.employee.model.Employee;
+import ait.employee.model.Manager;
+import ait.employee.model.SalesManager;
+import ait.employee.model.WageEmployee;
+
+public class EmployeeAppl {
+    public static void main(String[] args) {
+        Employee[] employees = new Employee[6];
+        employees[0] = new Manager(1000,"John", "Smith", 160, 3000, 5);
+        employees[1] = new WageEmployee(2000,"Mary", "Smith", 160, 15);
+        employees[2] = new SalesManager(3000,"Peter", "Jackson", 160, 20000, 0.1);
+        employees[3] = new SalesManager(4000,"Rabindranante", "Anand", 80, 30000, 0.1);
+        //employees[4] = new Employee(5000, "Ivan", "Balda", 160); can't because of abstract class
+        printArray(employees);
+        double total = totalSalary(employees);
+        System.out.println("Total salary " + total);
+        total = totalSales(employees);
+        System.out.println("Total salas " + total);
+    }
+
+    private static double totalSales(Employee[] employees) {
+        double sum = 0;
+        for (int i = 0; i < employees.length; i++) {
+            if(employees[i] instanceof SalesManager){
+                SalesManager sm = (SalesManager) employees[i];
+                sum += sm.getSalesValue();
+            }
+        }
+        return sum;
+    }
+
+    private static double totalSalary(Employee[] employees){
+        double sum = 0;
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i] != null){
+                sum += employees[i].calcSalary();
+            }
+        }
+        return sum;
+    }
+
+    private static void printArray(Employee[] employees) {
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i] != null){
+            System.out.println(employees[i]);
+            }
+        }
+    }
+}
